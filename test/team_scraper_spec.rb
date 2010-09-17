5a5997e50a6744d969fabbfd28c5e2dcdd7dab3b
Grant Gumina <grant.gumina@atomicobject.com>
2010-09-16 20:08:12 -0400

    Not working. Must rewrite algorithm that builds arrays.

--- test/team_scraper_spec.rb
+++ test/team_scraper_spec.rb
@@ -3,9 +3,12 @@ describe TeamInfoRequest, "A request for team information" do
   @@team_stats = ['38', '2188', '11', '48.00', '12.00','4.00']
   request = TeamInfoRequest.new('http://www2.usfirst.org/2010comp/events/gt/rankings.html')
   match_result_request = TeamInfoRequest.new('http://www2.usfirst.org/2010comp/events/GT/matchresults.html')
-  
+  num_of_team_request = TeamInfoRequest.new('http://www2.usfirst.org/2010comp/events/GT/schedulequal.html')
+
   match_result_request.findData('<TR style="background-color:#FFFFFF;" >', '</table>', />(.+)</)
   request.findData('<TR style="background-color:#FFFFFF;" >', '</table>', />(.+)</)
+  num_of_team_request.findData('<TR style="background-color:#FFFFFF;" >', '</table>', />(.+)</)
+
 #----------------------------------------------------------------------------
   it "should return team's stats when given a rank" do
     request.getStatsByRank(38).should == @@team_stats
@@ -17,7 +20,6 @@ describe TeamInfoRequest, "A request for team information" do
   end
 
   it "should return the team's match results" do
-    puts match_result_request.getTeamMatchResults(1918)
-    # match_result_request.getTeamMatchResults(1918).should include(["9:18 AM", "50", "1918", "1783", "2719", "3114", "3357", "2245", "5", "0", "12:36 PM", "74", "1918", "904", "2771", "1711", "1596", "2474", "4", "4"], ["12:33 PM", "9", "1896", "1918", "1711", "2000", "3423", "107", "5", "0"])
+    match_result_request.getTeamMatchResults(1918).should include(["9:18 AM", "50", "1918", "1783", "2719", "3114", "3357", "2245", "5", "0", "Red", "12:36 PM", "74", "1918", "904", "2771", "1711", "1596", "2474", "4", "4", "Red"], ["12:33 PM", "9", "1896", "1918", "1711", "2000", "3423", "107", "5", "0", "Red"])
   end
 end
