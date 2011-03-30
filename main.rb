6dbe01200bf697d3025c6dc126e83c55e51b513e
Grant J. Gumina <grantgumina@gmail.com>
2011-03-30 16:37:42 -0400

    Added overall rankings on team page. Increased width to 1000px.

--- main.rb
+++ main.rb
@@ -37,9 +37,7 @@ get '/' do
     t1 = Thread.new do
     req = EventListInfoRequest.new('http://www.thebluealliance.com/events/')
       @event_names = req.findData('<ul class="infoList">', '</ul>', /">(.*?)\s</)
-      # @event_names = req.findData('<ul class="infoList">', '</ul>', /">(.*)</)
       @event_names.each do |en|
-        # en.gsub!(/<span(.*?)>/,'')
         en.strip!
       end
       @event_names.delete("Oklahoma City, OK")
@@ -54,7 +52,6 @@ get '/' do
     t2.join
 
     @event_urls = create_event_urls(@event_short_names)
-    puts @event_urls
     haml :index 
   rescue
     haml :error, :layout => false
@@ -88,21 +85,34 @@ get '/event/:short_name' do |@event|
 end
 
 get '/event/:short_name/team/:team_number' do
-  tn_request = TeamListInfoRequest.new("https://my.usfirst.org/myarea/index.lasso?page=teamlist&menu=false&event=#{params[:short_name]}&year=2011&event_type=FRC")
-  @team_numbers = tn_request.findData('<tr bgcolor="#FFFFFF">', '</table>', /">(.+)<\/a/)
-  @team_numbers.map! do |x| 
-    x.to_i
+  begin
+    tn_request = TeamListInfoRequest.new("https://my.usfirst.org/myarea/index.lasso?page=teamlist&menu=false&event=#{params[:short_name]}&year=2011&event_type=FRC")
+    @team_numbers = tn_request.findData('<tr bgcolor="#FFFFFF">', '</table>', /">(.+)<\/a/)
+    @team_numbers.map! do |x| 
+      x.to_i
+    end
+
+    ranking_request = TeamInfoRequest.new("http://www2.usfirst.org/2011comp/events/#{params[:short_name]}/rankings.html")
+    match_request = TeamInfoRequest.new("http://www2.usfirst.org/2011comp/events/#{params[:short_name]}/matchresults.html")
+
+    ranking_request.findData('<TR style="background-color:#FFFFFF;" >', '</table>', />(.+)</)
+    match_request.findData('<TR style="background-color:#FFFFFF;" >', '</table>', />(.*?)</)
+
+    @team_number = params[:team_number]
+    @team_stats = ranking_request.getStatsByTeamNumber(params[:team_number])
+    @team_match_results = match_request.getTeamMatchResults(params[:team_number])
+    
+    @results = []
+
+    @team_urls = create_team_urls(@event, @team_numbers)  
+    @team_numbers.length.times do |rank|
+      rank += 1
+      @results.push(ranking_request.getStatsByRank(rank))
+    end
+
+
+    haml :team
+  rescue
+    haml :error
   end
-
-  request = TeamInfoRequest.new("http://www2.usfirst.org/2011comp/events/#{params[:short_name]}/rankings.html")
-  match = TeamInfoRequest.new("http://www2.usfirst.org/2011comp/events/#{params[:short_name]}/matchresults.html")
-
-  request.findData('<TR style="background-color:#FFFFFF;" >', '</table>', />(.+)</)
-  match.findData('<TR style="background-color:#FFFFFF;" >', '</table>', />(.*?)</)
-
-  @team_number = params[:team_number]
-  @team_stats = request.getStatsByTeamNumber(params[:team_number])
-  @team_match_results = match.getTeamMatchResults(params[:team_number])
-
-  haml :team
 end
